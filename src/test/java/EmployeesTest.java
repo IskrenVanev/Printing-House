dc13172976f9d57d269d2305c1cd6f58584b3427
isko02 <iskren3009@abv.bg>
2023-05-06 14:21:46 +0300

    Bug fix.

--- src/test/java/EmployeesTest.java
+++ src/test/java/EmployeesTest.java
@@ -7,22 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmployeesTest {
+    //This is tested in PrintingHouseTest class!
 
-    @Test
-    void calculateSalaryManager(){
-        PrintingHouse printingHouse = new PrintingHouse("IskoPHouse",100000,20000, 1000, 5);
-        Employee employee = new Manager("Ivan",10);
-        double expectedSalary = 1100;
-        double actualSalary = 1100;
-        assertEquals(expectedSalary, actualSalary);
-    }
+  //  @Test
+  //  void calculateSalaryManager(){
+  //      PrintingHouse printingHouse = new PrintingHouse("IskoPHouse",100000,20000, 1000, 5);
+  //      Employee employee = new Manager("Ivan",10);
+  //      printingHouse.hireEmployee(employee);
+  //      double expectedSalary = 1100;
+//
+  //      assertEquals(expectedSalary, employee.calculateSalary(1000));
+  //  }
+
+
+
     @Test
     void IdEmployeesTest(){
         Employee employee = new Manager("Ivan",10);
         Employee employee2 = new PrintingMachineOperator("Ivan");
         Employee employee3 = new Employee("Ivan");
         int expectedId = 3;
-        int actualId = 3;
+        int actualId = employee3.getId();
         assertEquals(expectedId, actualId);
     }
 }
